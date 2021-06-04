852fab1537f16293857944983ff1cb3ea8b93ca9
javin1102 <61945507+javin1102@users.noreply.github.com>
2021-06-04 14:11:09 +0700

    additional statement

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,15 +1,25 @@
-import ExpenseItem from "./ExpenseItem";
 import "./Expenses.css";
 import Card from "../UI/Card";
 import ExpenseFilter from "./ExpenseFilter";
 import { useState } from "react";
+import ExpensesList from "./ExpensesList";
 
 function Expenses(props) {
   const [filterYear, setFilterYear] = useState("2020");
   const filterChangeEvent = (value) => {
     setFilterYear(value);
   };
+  const filteredExpense = props.items.filter((e) => {
+    return e.date.getFullYear() === Number(filterYear);
+  });
   // <ExpenseItem title={e.title} amount={e.amount} date={e.date}/>
+  /*<ExpenseItem
+  key={e.id}
+  title={e.title}
+  amount={e.amount}
+  date={e.date}
+/>*/
+
   return (
     <div>
       <Card className="expenses">
@@ -17,14 +27,12 @@ function Expenses(props) {
           selected={filterYear}
           onFilterChange={filterChangeEvent}
         />
-        {props.items.map((e) => {
-          return (
-            <ExpenseItem title={e.title} amount={e.amount} date={e.date} />
-          );
-        })}
+        <ExpensesList items={filteredExpense} />
       </Card>
     </div>
   );
 }
 
 export default Expenses;
+// const b = [3, 32, 43, 43, 5];
+// b.filter((e) => e > 5).map((e) => console.log(e));
